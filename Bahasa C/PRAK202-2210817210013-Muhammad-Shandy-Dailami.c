d9d1a3048ced53f1e95ff203b4e589a674844128
gandul_tempest <sshandi08@gmail.com>
2022-10-17 10:44:56 +0800

    Tugas 3

--- Bahasa C/PRAK202-2210817210013-Muhammad-Shandy-Dailami.c
+++ Bahasa C/PRAK202-2210817210013-Muhammad-Shandy-Dailami.c
@@ -8,7 +8,7 @@ int main(){
     printf("Masukkan angka pertama: ");
     scanf("%f", &nilai2);
     result = nilai1 + nilai2;
-    printf("Hasil dari penjumlahan nilai pertama \"%.0f\" dan nilai kedua \"%.1f\" adalah \"%.2f\"",nilai1, nilai2, result);
+    printf("Hasil dari penjumlahan nilai pertama \"%.2f\" dan nilai kedua \"%.1f\" adalah \"%.2f\"",nilai1, nilai2, result);
 
     return 0;
 }
